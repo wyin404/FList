dfa6e37aa588b08a91a274fc5970d1e3305f9788
wyin404 <wyin5100@outlook.com>
2026-08-04 23:54:12 +0800

    a

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -13,6 +13,27 @@ export default defineUserConfig({
   description: '附属于wyin5100.top',
   // 页面 <head> 标签内添加的额外标签。 不要修改/logo.png可以替换掉这个文件，删除logo.png会导致构建出错。
   head: [['link', { rel: 'icon', href: '/logo.png' }]],
+   [
+      'script',
+      {},
+      `
+        (function() {
+          var hour = new Date().getHours();
+          var bgUrl = '';
+          if (hour >= 6 && hour < 18) {
+            bgUrl = '/background2.jpg';
+          } else if (hour >= 18 && hour < 24) {
+            bgUrl = '/background.jpg';
+          } else {
+            bgUrl = '/background.jpg';
+          }
+          document.body.style.backgroundImage = 'url(' + bgUrl + ')';
+          document.body.style.backgroundSize = 'cover';
+          document.body.style.backgroundPosition = 'center';
+          document.body.style.backgroundAttachment = 'fixed';
+        })();
+      `
+    ],
   // 页面预加载，所有其它页面所需的文件都会被预拉取。这对于小型站点来说是十分有帮助的，因为它会大大提升页面切换的速度。但是在你的网站有很多页面时不建议你这么做。
   // 简单来说就是，如果你的文件不多就可以打开这个选项，可以大大提高页面切换的速度，如果文件非常多就不建议打开。建议超过100个文件就不要打开这个选项。
   shouldPrefetch: true,
